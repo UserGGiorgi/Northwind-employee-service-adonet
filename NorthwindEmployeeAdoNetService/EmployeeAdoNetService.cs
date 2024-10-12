ff6fabb81e27baefa94be6ff5d85ccc2174e3d2c
Abesdzegiorgi <abesdzegiorgi@gmail.com>
2024-10-12 15:20:53 +0400

    second part

--- NorthwindEmployeeAdoNetService/EmployeeAdoNetService.cs
+++ NorthwindEmployeeAdoNetService/EmployeeAdoNetService.cs
@@ -93,7 +93,8 @@ public sealed class EmployeeAdoNetService
             connection.Open();
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = $"SELECT  EmployeeID,FirstName,LastName,Title FROM Employees Where EmployeeID={employeeId}";
+                command.CommandText = $"SELECT EmployeeID, FirstName, LastName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension, Notes, ReportsTo, PhotoPath FROM Employees WHERE EmployeeID = {employeeId}";
+
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
@@ -102,10 +103,24 @@ public sealed class EmployeeAdoNetService
                         {
                             FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
                             LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                            Title = reader.IsDBNull(reader.GetOrdinal("Title")) ? null : reader.GetString(reader.GetOrdinal("Title")) // Handle nullable Title
+                            Title = reader.IsDBNull(reader.GetOrdinal("Title")) ? null : reader.GetString(reader.GetOrdinal("Title")),
+                            TitleOfCourtesy = reader.IsDBNull(reader.GetOrdinal("TitleOfCourtesy")) ? null : reader.GetString(reader.GetOrdinal("TitleOfCourtesy")),
+                            BirthDate = reader.IsDBNull(reader.GetOrdinal("BirthDate")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("BirthDate")),
+                            HireDate = reader.IsDBNull(reader.GetOrdinal("HireDate")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("HireDate")),
+                            Address = reader.IsDBNull(reader.GetOrdinal("Address")) ? null : reader.GetString(reader.GetOrdinal("Address")),
+                            City = reader.IsDBNull(reader.GetOrdinal("City")) ? null : reader.GetString(reader.GetOrdinal("City")),
+                            Region = reader.IsDBNull(reader.GetOrdinal("Region")) ? null : reader.GetString(reader.GetOrdinal("Region")),
+                            PostalCode = reader.IsDBNull(reader.GetOrdinal("PostalCode")) ? null : reader.GetString(reader.GetOrdinal("PostalCode")),
+                            Country = reader.IsDBNull(reader.GetOrdinal("Country")) ? null : reader.GetString(reader.GetOrdinal("Country")),
+                            HomePhone = reader.IsDBNull(reader.GetOrdinal("HomePhone")) ? null : reader.GetString(reader.GetOrdinal("HomePhone")),
+                            Extension = reader.IsDBNull(reader.GetOrdinal("Extension")) ? null : reader.GetString(reader.GetOrdinal("Extension")),
+                            Notes = reader.IsDBNull(reader.GetOrdinal("Notes")) ? null : reader.GetString(reader.GetOrdinal("Notes")),
+                            ReportsTo = reader.IsDBNull(reader.GetOrdinal("ReportsTo")) ? (int?)null : reader.GetInt32(reader.GetOrdinal("ReportsTo")),
+                            PhotoPath = reader.IsDBNull(reader.GetOrdinal("PhotoPath")) ? null : reader.GetString(reader.GetOrdinal("PhotoPath"))
                         };
                     }
                 }
+
             }
         }
         throw new EmployeeServiceException($"Employee with ID {employeeId} not found.");
@@ -138,9 +153,8 @@ public sealed class EmployeeAdoNetService
                 command.CommandText = @"
                 INSERT INTO Employees (FirstName, LastName, Title)
                 VALUES (@FirstName, @LastName, @Title);
-                SELECT last_insert_rowid();"; // Gets the ID of the newly inserted employee
+                SELECT last_insert_rowid();";
 
-                // Add parameters to prevent SQL injection
                 var firstNameParam = command.CreateParameter();
                 firstNameParam.ParameterName = "@FirstName";
                 firstNameParam.Value = employee.FirstName;
@@ -153,14 +167,13 @@ public sealed class EmployeeAdoNetService
 
                 var titleParam = command.CreateParameter();
                 titleParam.ParameterName = "@Title";
-                titleParam.Value = employee.Title ?? (object)DBNull.Value; // Handle nullable Title
+                titleParam.Value = employee.Title ?? (object)DBNull.Value;
                 command.Parameters.Add(titleParam);
 
                 try
                 {
-                    // Execute the command and get the last inserted ID
                     var newId = (long)command.ExecuteScalar();
-                    return newId; // Return the ID of the newly added employee
+                    return newId;
                 }
                 catch (Exception ex)
                 {
@@ -191,7 +204,6 @@ public sealed class EmployeeAdoNetService
             {
                 command.CommandText = "DELETE FROM Employees WHERE EmployeeID = @EmployeeID";
 
-                // Add parameter to prevent SQL injection
                 var employeeIdParam = command.CreateParameter();
                 employeeIdParam.ParameterName = "@EmployeeID";
                 employeeIdParam.Value = employeeId;
@@ -200,11 +212,9 @@ public sealed class EmployeeAdoNetService
                 try
                 {
                     command.ExecuteNonQuery();
-                    // No exception is thrown for a non-existent employee
                 }
                 catch (Exception ex)
                 {
-                    // Consider logging the exception here for diagnostics
                     throw new EmployeeServiceException("An error occurred while removing the employee.", ex);
                 }
             }
@@ -217,7 +227,7 @@ public sealed class EmployeeAdoNetService
     /// </summary>
     /// <param name="employee">The employee object containing updated information.</param>
     /// <exception cref="EmployeeServiceException">Thrown when there is an issue updating the employee record.</exception>
-    public void UpdateEmployee(Employee employee)
+    public async Task UpdateEmployee(Employee employee)
     {
         if (employee == null)
         {
@@ -236,32 +246,132 @@ public sealed class EmployeeAdoNetService
 
             using (var command = connection.CreateCommand())
             {
-                command.CommandText = "UPDATE Employees SET Name = @Name, Position = @Position, Salary = @Salary WHERE EmployeeID = @EmployeeID";
+                command.CommandText = @"
+                UPDATE Employees 
+                SET 
+                    FirstName = @FirstName, 
+                    LastName = @LastName, 
+                    Title = @Title, 
+                    TitleOfCourtesy = @TitleOfCourtesy, 
+                    BirthDate = @BirthDate, 
+                    HireDate = @HireDate, 
+                    Address = @Address, 
+                    City = @City, 
+                    Region = @Region, 
+                    PostalCode = @PostalCode, 
+                    Country = @Country, 
+                    HomePhone = @HomePhone, 
+                    Extension = @Extension, 
+                    Notes = @Notes, 
+                    ReportsTo = @ReportsTo, 
+                    PhotoPath = @PhotoPath
+                WHERE EmployeeID = @EmployeeID";
 
-                command.Parameters.Add(CreateParameter("@EmployeeID", employee.Id));
-                command.Parameters.Add(CreateParameter("@Name", employee.FirstName));
-                command.Parameters.Add(CreateParameter("@Position", employee.LastName));
-                if(employee.Title != null)
-                command.Parameters.Add(CreateParameter("@Salary", employee.Title));
+                
+                var employeeIdParam = command.CreateParameter();
+                employeeIdParam.ParameterName = "@EmployeeID";
+                employeeIdParam.Value = employee.Id;
+                command.Parameters.Add(employeeIdParam);
 
-                int rowsAffected = command.ExecuteNonQuery();
+                var firstNameParam = command.CreateParameter();
+                firstNameParam.ParameterName = "@FirstName";
+                firstNameParam.Value = employee.FirstName;
+                command.Parameters.Add(firstNameParam);
+
+                var lastNameParam = command.CreateParameter();
+                lastNameParam.ParameterName = "@LastName";
+                lastNameParam.Value = employee.LastName;
+                command.Parameters.Add(lastNameParam);
+
+                var titleParam = command.CreateParameter();
+                titleParam.ParameterName = "@Title";
+                titleParam.Value = employee.Title ?? ""; 
+                command.Parameters.Add(titleParam);
+
+                var titleOfCourtesyParam = command.CreateParameter();
+                titleOfCourtesyParam.ParameterName = "@TitleOfCourtesy";
+                titleOfCourtesyParam.Value = employee.TitleOfCourtesy ?? (object)DBNull.Value;
+                command.Parameters.Add(titleOfCourtesyParam);
+
+                var birthDateParam = command.CreateParameter();
+                birthDateParam.ParameterName = "@BirthDate";
+                birthDateParam.Value = employee.BirthDate ?? (object)DBNull.Value;
+                command.Parameters.Add(birthDateParam);
+
+                var hireDateParam = command.CreateParameter();
+                hireDateParam.ParameterName = "@HireDate";
+                hireDateParam.Value = employee.HireDate ?? (object)DBNull.Value;
+                command.Parameters.Add(hireDateParam);
+
+                var addressParam = command.CreateParameter();
+                addressParam.ParameterName = "@Address";
+                addressParam.Value = employee.Address ?? (object)DBNull.Value;
+                command.Parameters.Add(addressParam);
+
+                var cityParam = command.CreateParameter();
+                cityParam.ParameterName = "@City";
+                cityParam.Value = employee.City ?? (object)DBNull.Value;
+                command.Parameters.Add(cityParam);
+
+                var regionParam = command.CreateParameter();
+                regionParam.ParameterName = "@Region";
+                regionParam.Value = employee.Region ?? (object)DBNull.Value;
+                command.Parameters.Add(regionParam);
+
+                var postalCodeParam = command.CreateParameter();
+                postalCodeParam.ParameterName = "@PostalCode";
+                postalCodeParam.Value = employee.PostalCode ?? (object)DBNull.Value;
+                command.Parameters.Add(postalCodeParam);
+
+                var countryParam = command.CreateParameter();
+                countryParam.ParameterName = "@Country";
+                countryParam.Value = employee.Country ?? (object)DBNull.Value;
+                command.Parameters.Add(countryParam);
+
+                var homePhoneParam = command.CreateParameter();
+                homePhoneParam.ParameterName = "@HomePhone";
+                homePhoneParam.Value = employee.HomePhone ?? (object)DBNull.Value;
+                command.Parameters.Add(homePhoneParam);
+
+                var extensionParam = command.CreateParameter();
+                extensionParam.ParameterName = "@Extension";
+                extensionParam.Value = employee.Extension ?? (object)DBNull.Value;
+                command.Parameters.Add(extensionParam);
+
+                var notesParam = command.CreateParameter();
+                notesParam.ParameterName = "@Notes";
+                notesParam.Value = employee.Notes ?? (object)DBNull.Value;
+                command.Parameters.Add(notesParam);
+
+                var reportsToParam = command.CreateParameter();
+                reportsToParam.ParameterName = "@ReportsTo";
+                reportsToParam.Value = employee.ReportsTo ?? (object)DBNull.Value;
+                command.Parameters.Add(reportsToParam);
+
+                var photoPathParam = command.CreateParameter();
+                photoPathParam.ParameterName = "@PhotoPath";
+                photoPathParam.Value = employee.PhotoPath ?? (object)DBNull.Value;
+                command.Parameters.Add(photoPathParam);
+
+                int rowsAffected = await command.ExecuteNonQueryAsync();
 
                 if (rowsAffected == 0)
                 {
-                    throw new EmployeeServiceException($"No employee record was updated. Check if the employee exists.");
+                    throw new EmployeeServiceException("Employee is not updated.");
                 }
             }
         }
     }
 
 
-    // Helper method to create parameters
+
+
     private IDbDataParameter CreateParameter(string name, object value)
     {
         var parameter = _dbFactory.CreateParameter();
          if(parameter.Value != null)
         parameter.ParameterName = name;
-        parameter.Value = value ?? DBNull.Value; // Handle null values
+        parameter.Value = value ?? DBNull.Value;
         return parameter;
     }
 
